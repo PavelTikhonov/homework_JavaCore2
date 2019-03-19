d45c1f637bbbce6334b7266bee8e44d04c3eb048
Pavel Tikhonov <aquilonis_7@mail.ru>
2019-03-19 18:24:05 +0300

    доработка потоков

--- src/homework/lesson5/Main.java
+++ src/homework/lesson5/Main.java
@@ -1,68 +1,42 @@
 package homework.lesson5;
 
-import java.util.Arrays;
-
 public class Main {
-    private static final int SIZE = 10000000;
-    private static final int HALF = SIZE / 2;
-    private static float[] arrFull = new float[SIZE];
-    private static float[] arrFull1 = new float[SIZE];
+
 
     public static void main(String[] args) {
-        for (int i = 0; i < SIZE; i++) {
-            arrFull[i] = 1;
-            arrFull1[i] = 1;
-        }
+
+        Thread t1 = new Thread(new MyRunnable());
+        Thread t2 = new Thread(new MyRunnable());
+        t1.start();
+        t2.start();
+/*
+        MyThread myThread = new MyThread();
 
         System.out.println("Время расчета значений элементов массива, мс:");
         long a = System.currentTimeMillis();
-        Main.calculateFullSize();
+        myThread.calculateFullSize();
         System.out.println("- arrFull (без применения инструментов многопоточности): " + (System.currentTimeMillis() - a));
 
         a = System.currentTimeMillis();
-        Main.calculateHalfSize();
+        myThread.calculateHalfSize();
         System.out.println("- arrFull1 (с применением инструментов многопоточности): " + (System.currentTimeMillis() - a));
 
-        if(Arrays.equals(arrFull, arrFull1)){
-            System.out.println("Массивы arrFull и arrFull1 идентичны");
-        } else {
-            System.out.println("Массивы arrFull и arrFull1 не идентичны");
+        myThread.equals();
+*/
+    }
+
+}
+
+class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+//            try {
+//                Thread.sleep(10);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
         }
-
     }
-
-    private static void expression(float[] aArr, int start, int len){
-        for (int i = 0; i < len; i++) {
-            aArr[i] = (float) (aArr[i] * Math.sin(0.2f + (float) ((start + i) / 5)) *
-                    Math.cos(0.2f + (float) ((start + i) / 5)) * Math.cos(0.4f + (float) ((start + i) / 2)));
-        }
-    }
-
-    private static void calculateFullSize() {
-        expression(arrFull, 0, arrFull.length);
-    }
-
-    private static void calculateHalfSize(){
-        float[] arr1 = new float[HALF];
-        float[] arr2 = new float[HALF];
-        System.arraycopy(arrFull1, 0, arr1, 0, HALF);
-        System.arraycopy(arrFull1, HALF, arr2, 0, HALF);
-
-        Runnable r1 = () -> expression(arr1, 0, arr1.length);
-        Runnable r2 = () -> expression(arr2, HALF, arr2.length);
-        Thread t1 = new Thread(r1);
-        Thread t2 = new Thread(r2);
-        t1.start();
-        t2.start();
-        try {
-            t1.join();
-            t2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.arraycopy(arr1, 0, arrFull1, 0, HALF);
-        System.arraycopy(arr2, 0, arrFull1, HALF, HALF);
-    }
-
 }
